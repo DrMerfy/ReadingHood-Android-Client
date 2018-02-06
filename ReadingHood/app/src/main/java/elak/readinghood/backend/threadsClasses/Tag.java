db8c605e303d6b3602c39bfd50dcdc1a659a1283
spyridon97 <spyridon97@hotmail.com>
2018-02-07 01:55:48 +0200

    fixing backend directory

---
+++ ReadingHood/app/src/main/java/elak/readinghood/backend/threadsClasses/Tag.java
@@ -0,0 +1,41 @@
+package elak.readinghood.backend.threadsClasses;
+
+/**
+ * @author Spiros
+ */
+public class Tag {
+    private int id, usages;
+    private String name;
+
+    /**
+     * @param id     is the id of the of tag
+     * @param usages is the usages of the tag
+     * @param name   is the name of the tag
+     */
+    public Tag(int id, int usages, String name) {
+        this.id = id;
+        this.usages = usages;
+        this.name = name;
+    }
+
+    /**
+     * @return the of the tag
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @return the usages of the tag
+     */
+    public int getUsages() {
+        return usages;
+    }
+
+    /**
+     * @return the name of the tag
+     */
+    public String getName() {
+        return name;
+    }
+}
