6e8f782e0553fa82812aac486b9293466f5448a3
spyridon97 <spyridon97@hotmail.com>
2018-02-07 01:22:16 +0200

    fixing backend directory

---
+++ ReadingHood/app/src/main/java/elak/readinghood/backend/profileClasses/Profile.java
@@ -0,0 +1,59 @@
+package elak.readinghood.backend.ProfileClasses;
+
+/**
+ * @author Spiros
+ */
+public class Profile {
+    protected int id;
+    protected String name, surname, department;
+
+    /**
+     * trivial constructor
+     */
+    public Profile() {
+
+    }
+
+    /**
+     * trivial constructor
+     *
+     * @param id         the id of the user
+     * @param name       the user given name
+     * @param surname    the user given surname
+     * @param department the user given department
+     */
+    public Profile(int id, String name, String surname, String department) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.department = department;
+    }
+
+    /**
+     * @return the id of the UserProfile
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @return the name of the user
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * @return the surname of the user
+     */
+    public String getSurname() {
+        return this.surname;
+    }
+
+    /**
+     * @return the department of the user
+     */
+    public String getDepartment() {
+        return this.department;
+    }
+}
