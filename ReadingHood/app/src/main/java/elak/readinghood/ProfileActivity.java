88329c96f56d3c861cf9cf63dcb4c2cd0ce75a69
alexandrinio <andalemic@csd.auth.gr>
2018-02-08 22:54:43 +0200

    adding profile activity

---
+++ ReadingHood/app/src/main/java/elak/readinghood/ProfileActivity.java
@@ -0,0 +1,15 @@
+package elak.readinghood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+    }
+}
